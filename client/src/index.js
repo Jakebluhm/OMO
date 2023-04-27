7ffd2eff1495d078977b12969bcb0ea15936eb53
unknown <bluhmj96@gmail.com>
2023-04-26 23:58:27 -0400

    Clean up begin

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import * as AWS from "aws-sdk";
-
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: "us-east-1:f698415d-522d-41c4-bfca-2cbc75de02ed",
-  RoleArn: "arn:aws:iam::426938983711:role/Cognito_DynamoPoolUnauth",
-});
-
-AWS.config.update({
-  region: "us-east-1",
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,27 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { v1 as uuid } from "uuid";
 import Modal from "react-modal";
-
 import "../ModalStyles/ModalStyle.css";
-
-import AWS from "aws-sdk";
-import { putData } from "../awsLib/awsFunctions.js";
 import PromptComponent from "../components/PromptComponent";
 
 const CreateRoom = (props) => {
-  const [promptClicked, setPromptClicked] = useState({});
-
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  const [items, setItems] = useState([]);
-
   const [name, setname] = useState("");
-  const [uuidNum, setuuid] = useState("");
 
   // Modal State
   const [isSearchingModalOpen, setIsOpen] = useState(false);
-  //const [modalIndex, setModalIndex] = useState(0)
-  const [selectedAnswer, setSelectedAnswer] = useState(-1); // 1 is omo, 0 is not omo
 
   const [userData, setUserData] = useState({
     TableName: "OMOUserQueue",
@@ -31,74 +18,6 @@ const CreateRoom = (props) => {
     },
   });
 
-  // useEffect(() => {
-  //     console.log('selectedAnswer was updated')
-  //     // console.log('selectAnswer')
-  //     // console.log(selectAnswer)
-
-  //     console.log('userData')
-  //     console.log(userData)
-
-  //     const params = {
-  //       TableName: "OMOUserQueue"
-  //     };
-
-  //     // dynamoDb.scan(params, (err, data) => {
-  //     //     if (err) {
-  //     //       console.error(err);
-  //     //     } else {
-  //     //         console.error('data.Items');
-  //     //         console.error(data.Items);
-  //     //       setItems(data.Items);
-  //     //     }
-  //     //   });
-
-  //     if(selectedAnswer !== -1){
-  //         console.log("dummyPrompts.find(element => element.id === modalIndex).uuid  --- Calling create" )
-  //         console.log(dummyPrompts.find(element => element.id === modalIndex).uuid)
-  //         //create(dummyPrompts.find(element => element.id === modalIndex).uuid);
-  //     }
-  //     // TODO:Check for valid answer, rooms will already have an omo and new omo should be prevented from entering
-
-  // }, [selectedAnswer]);
-
-  // This runs on mount and establishes a listener for a websocket signal from aws lambda
-  // That tells the client the url of the Room this user has been matched to.
-  // useEffect(() => {
-  //   const userData = {
-  //     TableName: "OMOUserQueue",
-  //     Item: {
-  //       user: uuid(),
-  //       prompts: [],
-  //     },
-  //   };
-
-  //   const ws = new WebSocket(
-  //     `wss://86sw6hpi28.execute-api.us-east-1.amazonaws.com/production/?userId=${
-  //       userData.Item.user
-  //     }&prompts=${JSON.stringify(userData.Item.prompts)}`
-  //   );
-
-  //   // const ws = new WebSocket(
-  //   //   "wss://86sw6hpi28.execute-api.us-east-1.amazonaws.com/production/"
-  //   // );
-
-  //   ws.onmessage = (event) => {
-  //     const message = JSON.parse(event.data);
-
-  //     if (message.action === "sendURL") {
-  //       const url = message.url;
-  //       // Redirect the user to the URL received from the Lambda function
-  //       window.location.href = url;
-  //     }
-  //   };
-
-  //   return () => {
-  //     if (ws && ws.readyState === WebSocket.OPEN) {
-  //       ws.close();
-  //     }
-  //   };
-  // }, []);
   // Callback for confirm under Name entry
   async function startSearch() {
     console.log("startSearch() Establishing WebSocket Connection");
@@ -181,7 +100,6 @@ const CreateRoom = (props) => {
   const handlePromptClick = (buttonLabel) => {
     console.log("-----------handlePromptClick-----");
     console.log(buttonLabel);
-    setPromptClicked(buttonLabel);
 
     // Adding answered prompts to userData
     setUserData((prevState) => {
@@ -220,12 +138,6 @@ const CreateRoom = (props) => {
   function toggleModal() {
     console.log("toggleModal()");
 
-    //Only set id on open
-    // if(!isSearchingModalOpen){
-    //     console.log('id')
-    //     console.log(id)
-    //     setModalIndex(id)
-    // }
     setIsOpen(!isSearchingModalOpen);
   }
 
@@ -234,61 +146,10 @@ const CreateRoom = (props) => {
     setIsOpen(!isSearchingModalOpen);
   }
 
-  // function selectAnswer(ans){
-  //     console.log("selectAnswer()")
-  //     console.log('ans')
-  //     console.log(ans)
-  //     setSelectedAnswer(ans)
-  //     //create();
-  // }
-
-  function create(id) {
-    console.log("create()");
-    const uid = uuid();
-    // console.log(name)
-    props.history.push(`/room/${id}`, {
-      playerName: name,
-      oddOneOut: selectedAnswer,
-      uid: uid,
-    });
-  }
-
-  // Callback for joining with a specific UUID
-  function join() {
-    console.log("join()");
-    // console.log('uuidNum.length')
-    // console.log(uuidNum.length)
-    if (uuidNum.length > 20) {
-      // console.log('uuid is 128 in length')
-    } else {
-      // console.log('uuid is NOT 128 in length')
-      return;
-    }
-    // console.log(uuid + ' < Joining ')
-    props.history.push(`/room/${uuidNum}`, { playerName: name });
-  }
-
-  // Callback for confirm under Name entry
-  // async function startSearch() {
-  //   console.log("startSearch()");
-
-  //   console.log("'name': " + name);
-
-  //   try {
-  //     const data = await dynamoDb.put(userData).promise();
-  //     console.log("Success", data);
-  //     console.log("toggling modal");
-  //     toggleModal();
-  //   } catch (err) {
-  //     console.log("Error", err);
-  //   }
-  // }
   const handleNameChange = (e) => {
     console.log(`New name value: ${e.target.value}`);
     const currentName = e.target.value;
-    //if (e.target && e.target.value) {
     setname(e.target.value);
-    //userData.Item['name'] = e.target.value
     setUserData((prevState) => ({
       ...prevState,
       Item: {
@@ -296,10 +157,6 @@ const CreateRoom = (props) => {
         name: currentName,
       },
     }));
-
-    // console.log('userData')
-    // console.log(userData)
-    //}
   };
 
   const style = {
@@ -311,39 +168,7 @@ const CreateRoom = (props) => {
     width: "..",
     height: "..",
   };
-  const listItemStyle = { padding: 10 };
 
-  // These will be read in from database
-  // TODO: UUID can be assigned when prompt is created
-  // const dummyPrompts = [
-  //   {
-  //     id: 0,
-  //     uuid: "abc123",
-  //     prompt: "Prompt 1",
-  //     question: "Are you a Kanye West Fan?",
-  //     omoAns: "No",
-  //     notOmoAns: "Yes",
-  //     count: 0,
-  //   },
-  //   {
-  //     id: 1,
-  //     prompt: "Prompt 2",
-  //     uuid: "xyz789",
-  //     question: "Are you apart of the LGBTQIA+ community and or non-bindary?",
-  //     omoAns: "No",
-  //     notOmoAns: "Yes",
-  //     count: 0,
-  //   },
-  //   {
-  //     id: 2,
-  //     prompt: "Prompt 3",
-  //     uuid: "jkl456",
-  //     question: "Are you married",
-  //     omoAns: "Yes",
-  //     notOmoAns: "No",
-  //     count: 0,
-  //   },
-  // ];
   const dummyPrompts = [
     {
       id: 0,
@@ -368,16 +193,10 @@ const CreateRoom = (props) => {
     },
   ];
 
-  // <button onClick={create}>{dummyPrompts[0]}</button>
   return (
     <div style={style}>
       <div>
-        <h2>AWS DynamoDB Document Client</h2>
-        {items.map((item) => (
-          <div key={item.id}>
-            <p>{item.user + " - " + item.name}</p>
-          </div>
-        ))}
+        <h2>OMO</h2>
       </div>
 
       <div
@@ -410,11 +229,6 @@ const CreateRoom = (props) => {
           Confirm
         </button>
       </div>
-      {/* <div style={{paddingBottom:50, display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center',}}>
-                <label style={{padding:5}}> Enter uuid</label>
-                <input type="text" placeholder="uuid" value={uuidNum} onChange={e => setuuid(e.target.value)}></input>
-                <button  onClick={join} key={'joinUUID'}>Join with UUID</button> 
-            </div>      */}
       <label style={{ padding: 5 }}>
         Answer one or more questions below to be able to play OMO
       </label>
