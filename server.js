7e076ecf075b5e0481617160cdfb1596c8925f26
unknown <bluhmj96@gmail.com>
2023-05-10 19:34:01 -0400

    Change user left to only go to appropriate room. Change how the all users message is brodcast to clients with the intention that it only goes to the correct room

--- server.js
+++ server.js
@@ -65,7 +65,8 @@ io.on("connection", (socket) => {
     // }
 
     //This needs to be sent to room specific socket, see above
-    socket.emit("all users", usersInThisRoom);
+    //socket.emit("all users", usersInThisRoom);
+    io.in(payload.roomID).emit("all users", usersInThisRoom);
   });
 
   socket.on("vote cast", (payload) => {
@@ -117,7 +118,7 @@ io.on("connection", (socket) => {
       room = room.filter((id) => id.socketID !== socket.id);
       users[roomID] = room;
     }
-    socket.broadcast.emit("user left", socket.id);
+    socket.broadcast.to(roomID).emit("user left", socket.id);
 
     console.log("Room after Disconnect:");
     console.log(room);
