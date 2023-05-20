4e26f3b12088cf1a73e905f2083d9035888f7a9a
unknown <bluhmj96@gmail.com>
2023-05-20 11:11:07 -0400

    Add debug to jsx

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -951,7 +951,7 @@ const Room = (props) => {
               alignItems: "center",
               height: "400px",
               width: "400px",
-              border: "0px solid rgba(255, 0, 0, 1)",
+              border: "3x solid rgba(255, 0, 0, 1)",
             }}
           >
             <StyledVideo
