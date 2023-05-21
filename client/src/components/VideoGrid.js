f9e3d582b1cf08b84380d0c2ac6f473b79e6dcc3
unknown <bluhmj96@gmail.com>
2023-05-21 12:54:07 -0400

    ui stuff

--- client/src/components/VideoGrid.js
+++ client/src/components/VideoGrid.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Video } from "../routes/Room"; // adjust the path to match your project structure
-
+// Base size of the squares, this can be adjusted as needed
+const BASE_SIZE = 50;
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
+  grid-template-columns: repeat(2, ${(props) => props.size}px);
+  grid-template-rows: repeat(2, ${(props) => props.size}px);
   gap: 10px;
-  height: 100vh; /* adjust as needed */
-  width: 100vw; /* adjust as needed */
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
 `;
 
 const GridItem = styled.div`
+  height: ${(props) => props.size}px;
+  width: ${(props) => props.size}px;
   border: 1px solid black;
   display: flex;
   justify-content: center;
@@ -30,16 +35,38 @@ const VideoGrid = ({
   gameInfo,
   handleVideoReady,
 }) => {
+  const [size, setSize] = useState(BASE_SIZE);
+
+  // Use an effect to resize the grid when the window resizes
+  useEffect(() => {
+    const handleResize = () => {
+      // Calculate the size for the squares so that they fill the viewport
+      const newSize = Math.min(window.innerHeight / 2, window.innerWidth / 2);
+
+      setSize(newSize - 10); // subtract the gap size
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Call the function once to set the initial size
+    handleResize();
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <GridContainer>
-      <GridItem>
+    <GridContainer size={size}>
+      <GridItem size={size}>
         <StyledVideo muted ref={userVideo} autoPlay playsInline />
       </GridItem>
       {filteredPeers.map((peer, index) => {
         if (index < 2) {
           // Only take the first two peers
           return (
-            <GridItem key={peer.peerID}>
+            <GridItem key={peer.peerID} size={size}>
               <div
                 style={{
                   display: "flex",
--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -15,8 +15,8 @@ const Container = styled.div`
 `;
 
 const StyledVideo = styled.video`
-  height: 400px;
-  width: 400px;
+  height: 100%;
+  width: 100%;
 `;
 
 const Timer = styled.div`
