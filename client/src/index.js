8f961b294d1e6ed9dfc4bf6e0c7b4599d9c3b580
unknown <bluhmj96@gmail.com>
2023-06-03 16:42:20 -0400

    Add sentry to yml, and setup source maps for sentry via github actions

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b246fc51e6ff44a9bbb3554294f2305d@o4505297430577152.ingest.sentry.io/4505297434640384",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.render(
   <React.StrictMode>
