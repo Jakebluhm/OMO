4de1a9a4a82661182cfcaa0869b2ad40f138a43a
Jakebluhm <bluhmj96@gmail.com>
2023-09-10 12:30:27 -0400

    Add prompts

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -214,6 +214,126 @@ const CreateRoom = (props) => {
         "What's your go-to rap song to sing along to?"
       ],
     },
+    {
+      id: 3,
+      uuid: "def123",
+      identityA: "Left-wing",
+      identityB: "Right-wing",
+      count: 0,
+      iceBreakers: [
+        "Who's your favorite political leader?",
+        "What's your stance on healthcare?",
+        "How do you feel about tax breaks?"
+      ],
+    }, 
+    {
+      id: 4,
+      uuid: "ghi789",
+      identityA: "iPhone",
+      identityB: "Android",
+      count: 0,
+      iceBreakers: [
+        "Which app store do you prefer?",
+        "Have you ever switched from one to the other?",
+        "What feature of your phone do you value the most?"
+      ],
+    },
+    {
+      id: 5,
+      uuid: "mno456",
+      identityA: "Climate Change Believer",
+      identityB: "Climate Change Skeptic",
+      count: 0,
+      iceBreakers: [
+        "What's your primary news source?",
+        "Have you ever been impacted by extreme weather?",
+        "How do you feel about renewable energy?"
+      ],
+    },
+    {
+      id: 6,
+      uuid: "pqr123",
+      identityA: "Mac",
+      identityB: "PC",
+      count: 0,
+      iceBreakers: [
+        "Which operating system do you find more user-friendly?",
+        "How often do you upgrade your computer?",
+        "What's your go-to software or application on your platform?"
+      ],
+    },
+    {
+        id: 7,
+        uuid: "stu456",
+        identityA: "Pro-gun control",
+        identityB: "Anti-gun control",
+        count: 0,
+        iceBreakers: [
+          "How do you feel about stricter background checks for gun purchases?",
+          "Have you ever taken a firearm safety course?",
+          "What's your perspective on concealed carry permits?"
+        ],
+    },
+    {
+        id: 8,
+        uuid: "vwx789",
+        identityA: "Pro-choice",
+        identityB: "Pro-life",
+        count: 0,
+        iceBreakers: [
+          "How do you feel about reproductive health education in schools?",
+          "Do you think the government should have a say in personal health decisions?",
+          "What are your thoughts on pregnancy prevention methods?"
+        ],
+    },
+    {
+        id: 9,
+        uuid: "yza012",
+        identityA: "Pro-vaccination",
+        identityB: "Anti-vaccination",
+        count: 0,
+        iceBreakers: [
+          "How do you stay informed about new vaccines or medical updates?",
+          "What's your stance on mandatory vaccinations for school admissions?",
+          "Have you or your family experienced any side effects from vaccines?"
+        ],
+    },
+    {
+        id: 10,
+        uuid: "bcd345",
+        identityA: "Traditional education advocate",
+        identityB: "Homeschooling/unschooling proponent",
+        count: 0,
+        iceBreakers: [
+          "How do you feel about standardized testing?",
+          "What do you think are the most important aspects of education?",
+          "How do you envision the future of learning?"
+        ],
+    },
+    {
+      id: 11,
+      uuid: "efg678",
+      identityA: "Cats",
+      identityB: "Dogs",
+      count: 0,
+      iceBreakers: [
+        "Which pet do you think requires more attention and why?",
+        "Have you had any memorable experiences with a cat or dog?",
+        "How do you feel about training or disciplining pets?"
+      ],
+  },
+  {
+      id: 12,
+      uuid: "hij901",
+      identityA: "Morning People",
+      identityB: "Night Owls",
+      count: 0,
+      iceBreakers: [
+        "What's the first thing you do after waking up?",
+        "Do you rely on an alarm to wake up or naturally rise?",
+        "What time of day do you feel most productive or creative?"
+      ],
+  } 
   ];
   
 
